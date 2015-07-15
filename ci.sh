654e2323e959e887b09cf79b64cd0f69b8e928e0
Darren Shepherd <darren@rancher.com>
2015-07-15 00:06:33 -0700

    Add CI scripts

---
+++ ci.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+IMAGE=ibuildthecloud/lets-chat:${BUILD_ID:-latest}
+
+docker build -t $IMAGE -f docker/Dockerfile .
+
+if [[ "$DOCKER_HUB_USER" && -n "$DOCKER_HUB_PASS" && -n "$DOCKER_HUB_EMAIL" ]]; then
+    docker login -u "$DOCKER_HUB_USER" -p "$DOCKER_HUB_PASS" -e "$DOCKER_HUB_EMAIL"
+
+    echo Pushing $IMAGE
+    docker push $IMAGE
+fi
+
+if [[ -n "$RANCHER_SECRET_KEY" ]]; then
+    sed 's!sdelements/lets-chat:latest!'$IMAGE'!' > docker-compose.yml
+    rancher-compose -p lets-chat-${BUILD_ID} up
+fi
