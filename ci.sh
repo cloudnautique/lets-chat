8499455f1e492e89aa878f0d45d834f6ac1908e0
Bill Maxwell <bill@rancher.com>
2015-08-12 21:42:25 -0700

    testing drone

--- ci.sh
+++ ci.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-IMAGE=ibuildthecloud/lets-chat:${BUILD_NUMBER:-latest}
+IMAGE=cloudnautique/lets-chat:${DRONE_BUILD_NUMBER:-latest}
+export IMAGE
+
+wrapdocker >/dev/null 2>&1
 
 docker build -t $IMAGE -f docker/Dockerfile .
 
@@ -15,5 +18,10 @@ fi
 if [[ -n "$RANCHER_SECRET_KEY" ]]; then
     mkdir -p dist
     sed 's!sdelements/lets-chat:latest!'$IMAGE'!' docker/docker-compose.yml > dist/docker-compose.yml
-    rancher-compose -f dist/docker-compose.yml -p lets-chat-${BUILD_NUMBER} up -d
+    curl -X GET -o rancher-compose.tar.gz -L https://github.com/rancher/rancher-compose/releases/download/v0.2.5/rancher-compose-linux-amd64-v0.2.5.tar.gz
+    ls ./
+    tar -xvzf ./rancher-compose.tar.gz -C /usr/bin/
+    ln -s /usr/bin/rancher-compose-v0.2.5/rancher-compose /usr/bin/rancher-compose
+    sed -i "s@APPIMAGE@${IMAGE}@" dist/docker-compose.yml
+    rancher-compose -f dist/docker-compose.yml -p lets-chat-${DRONE_BUILD_NUMBER} up -d
 fi
